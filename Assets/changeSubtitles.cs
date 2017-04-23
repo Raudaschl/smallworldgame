5d64ca46d132a5014b8d2ea2b2a118254c6aeb3d
Adrian Raudaschl <raudaschl@gmail.com>
2017-04-23 16:17:37 +0100

    Added subtitles code

---
+++ Assets/changeSubtitles.cs
@@ -0,0 +1,216 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class changeSubtitles : MonoBehaviour {
+
+	private GameObject subtitlesBackdrop;
+	private GameObject subtitleCharacter;
+	private Text uiLabel;
+	private enviromentAudioVol controlEnviromentVol;
+	private bool firstSceneSpeech;
+	private IEnumerator coroutine;
+
+	public int currentdialoguenum;
+
+	public GameObject eventController;
+	private bool speaking;
+
+
+	// Use this for initialization
+	void Start () {
+		firstSceneSpeech = false;
+
+		uiLabel = GetComponent<Text> ();
+
+		subtitlesBackdrop = GameObject.FindGameObjectWithTag ("subtitle-backdrop");
+		subtitleCharacter = GameObject.FindGameObjectWithTag ("subtitleCharacter");
+
+		hideSubtitles (true);
+
+		controlEnviromentVol = gameObject.GetComponent<enviromentAudioVol> ();
+
+	}
+
+
+
+	public void setText(float waitTime, string subtitle){
+
+		coroutine = WaitAndPrint (waitTime, subtitle);
+		StartCoroutine(coroutine);
+
+	}
+
+	public void setTrigger(float waitTime, string triggerText){
+
+		if (eventController != null) {
+			Debug.Log (triggerText);
+			eventController.BroadcastMessage ("dialogueCompleteTrigger", triggerText);
+			StartCoroutine (coroutine);
+		}
+
+	}
+
+	private IEnumerator WaitAndPrint(float waitTime, string subtitle) {
+
+
+		yield return new WaitForSeconds(waitTime);
+
+		uiLabel.text = subtitle;
+
+		if (uiLabel.text == " ") {
+
+			hideSubtitles(false);
+
+		}
+	}
+
+	private void finishedSpeaking(){
+		Debug.Log ("finished speaking");
+
+		controlEnviromentVol.volumeControl (false);
+
+		speaking = false;
+	}
+
+	private void startedSpeaking(){
+		Debug.Log ("started speaking");
+		controlEnviromentVol.volumeControl (true);
+		speaking = true;
+	}
+
+
+	//Controls subtitle visibility
+
+	public void showSubtitles(int currentdialoguenumSent){
+		showSubtitleBackdrop();
+		uiLabel.text = "";
+		//		Debug.Log (firstSceneSpeech);
+
+		startedSpeaking ();
+
+
+		currentdialoguenum = currentdialoguenumSent;
+	}
+
+	public void hideSubtitles(bool start){
+
+
+		hideSubtitleBackdrop ();
+		hideText ();
+
+		//Broadcast that dialogue has ended
+		if (!start) {
+			if (eventController != null) {
+				eventController.BroadcastMessage ("dialogueCompleteTrigger", currentdialoguenum.ToString());
+				finishedSpeaking ();
+			}
+
+		}
+	}
+
+
+	public bool isSpeaking(){
+		if (speaking == true){
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+
+	private void showSubtitleBackdrop(){
+		subtitlesBackdrop.GetComponent<Image>().enabled = true;
+		subBgAnimateIn();
+
+		if (subtitleCharacter != null){
+			subCharacterAnimateIn();
+		}
+
+	}
+
+	private void hideSubtitleBackdrop(){
+		subBgAnimateOut();
+
+		if (subtitleCharacter != null){
+			subCharacterAnimateOut();
+		}
+
+
+	}
+
+	public void showSubtitleTextAndBackdrop(){
+
+		if (speaking == true){
+			subBgAnimateIn();
+			showText();
+		}
+
+
+
+	}
+
+	public void hideSubtitleTextAndBackdrop(){
+
+		if (speaking == true){
+			subBgAnimateOut();
+			hideText();
+		}
+
+
+
+	}
+
+
+	private void subBgAnimateIn(){
+		Vector3 test = new Vector3(5.8f,0.8f,1f);
+
+//		iTween.ScaleTo(subtitlesBackdrop, iTween.Hash(
+//			"scale"    , test,
+//			"time"    , 0.3f,
+//			"easeType", iTween.EaseType.easeInOutQuint
+//		));
+	}
+
+	private void subCharacterAnimateIn(){
+		Vector3 test = new Vector3(1,1,1);
+
+//		iTween.ScaleTo(subtitleCharacter, iTween.Hash(
+//			"scale"    , test,
+//			"time"    , 1,
+//			"easeType", iTween.EaseType.easeInOutQuint
+//		));
+	}
+
+	private void subBgAnimateOut(){
+		Vector3 test = new Vector3(0,0,0);
+
+//		iTween.ScaleTo(subtitlesBackdrop, iTween.Hash(
+//			"scale"    , test,
+//			"time"    , 0.3f
+//		));
+	}
+
+	private void subCharacterAnimateOut(){
+		Vector3 test = new Vector3(0,0,0);
+//		iTween.ScaleTo(subtitleCharacter, iTween.Hash(
+//			"scale"    , test,
+//			"time"    , 0.5f
+//		));
+	}
+
+	public void hideText(){
+		Color c = uiLabel.color;
+		c.a = 0;
+		uiLabel.color = c;
+	}
+
+	public void showText(){
+		Color c = uiLabel.color;
+		c.a = 1;
+		uiLabel.color = c;
+	}
+		
+
+
+}
---
+++ Assets/subtitleControl.cs
@@ -0,0 +1,219 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Xml;
+using System.IO;
+
+public class subtitleControl : MonoBehaviour {
+
+
+	//	public string bubbleText;
+
+	private TextAsset languageTextFile;
+
+	private string langSelectXml;
+
+	private GameObject subtitles;
+
+	private float delayedSubtitleDisappear = 0.6f;
+
+	private AudioSource audioSource;
+
+	private string path;
+
+	public string language = "english";
+
+	private changeSubtitles changeSubtitles;
+	private string textData;
+
+	public int sceneNumber;
+	private string sceneValueString;
+
+
+	private bool toggleSubtitles = true;
+
+	void Awake(){
+		languageTextFile = Resources.Load ("english") as TextAsset;
+	}
+
+	void Start() {
+
+
+
+		audioSource = GetComponent<AudioSource>();
+
+		subtitles = GameObject.FindGameObjectWithTag ("subtitles");
+
+//		Debug.Log (subtitles);
+		changeSubtitles = subtitles.GetComponent<changeSubtitles> ();
+
+
+		selectScene ();
+		//Set Language
+
+		languageSwitch (language);
+
+		textData = languageTextFile.text;
+
+	}
+
+	void selectScene() {
+
+		sceneValueString = gameObject.GetComponent<sceneArrayControl> ().determineScene (sceneNumber);
+		print (sceneValueString);
+
+	}
+
+
+	//Set Language
+	void languageSwitch(string language) {
+		var prevLanguagetSelect = language.ToLower ();
+		switch (prevLanguagetSelect) {
+		case "english":
+			langSelectXml = prevLanguagetSelect;
+			break;
+		case "french":
+			langSelectXml = prevLanguagetSelect;
+
+			break;
+		default:
+			Debug.Log ("Uh oh something is wrong with language select!");
+			break;
+		}
+
+	}
+
+
+
+	//dialogue - Parse the XMl document 
+	private void ParseScript(string xmlData, int narrationNum) {
+		XmlDocument xmlDoc = new XmlDocument();
+		xmlDoc.Load( new StringReader(xmlData) );
+
+		string xmlPathPatternAudio = "//scene" + sceneNumber.ToString() + "/narration" + narrationNum.ToString() + "/audio";
+		XmlNodeList myNodeListAudio = xmlDoc.SelectNodes(xmlPathPatternAudio );
+
+		foreach (XmlNode node in myNodeListAudio) {
+
+
+			AudioClip testAudio = Resources.Load (sceneValueString + "/audio/" + node.InnerText.ToString()) as AudioClip;
+
+			audioSource.clip = testAudio;
+			audioSource.Play ();
+		}
+
+
+
+		string xmlPathPattern = "//scene" + sceneNumber.ToString() + "/narration" + narrationNum.ToString() + "/lines/line";
+		XmlNodeList myNodeList = xmlDoc.SelectNodes(xmlPathPattern );
+
+
+		int i = 0;
+		foreach (XmlNode node in myNodeList) {
+			i++;
+			int totalNodes = myNodeList.Count;
+
+			float waitTime = float.Parse (node.SelectSingleNode ("timer").InnerText);
+			string innerText = node.SelectSingleNode("textBub").InnerText;
+			string triggerText = node.SelectSingleNode("trigger").InnerText;
+
+			StartCoroutine(WaitAndPrint (waitTime, innerText, triggerText));
+
+			//Remember to update the scenenArrayControl code you idiot
+			if (totalNodes == i) {
+
+				StartCoroutine(WaitAndPrint (audioSource.clip.length + delayedSubtitleDisappear, " ", triggerText));
+
+			}
+
+		}
+
+
+
+	}
+
+	private IEnumerator WaitAndPrint(float waitTime, string innerText, string triggerText) {
+
+		yield return new WaitForSeconds(waitTime);
+
+		BubbleRecordString (waitTime, innerText, triggerText);
+
+
+	}
+
+
+	//Loop Load lines from narration block
+	private void BubbleRecordString (float waitTime, string innerText, string triggerText) {
+		//		Debug.Log (waitTime);
+
+		if (innerText != "") {
+			Debug.Log (innerText);
+			changeSubtitles.setText (0f, innerText);
+		} else if (triggerText != "") {
+
+			changeSubtitles.setTrigger(0f, triggerText);
+		}
+
+	}
+
+
+
+	//Play dialogue lines with audio
+	public void playDialogue(int value){
+
+		if (value != 0){
+			stopTheSubtitles ();
+		}
+
+		changeSubtitles.showSubtitles (value);
+
+
+		toggleSubtitlesControl();
+
+
+		ParseScript( textData, value );
+
+
+	}
+
+
+	public void stopTheSubtitles () {
+
+		StopAllCoroutines ();
+		audioSource.Stop ();
+		changeSubtitles.hideSubtitles (false);
+
+	}
+
+	void toggleSubtitlesControl(){
+
+		if (!toggleSubtitles) {
+			changeSubtitles.hideText ();
+		} else {
+			changeSubtitles.showText ();
+		}
+	}
+
+
+	void Update () {
+
+		//Cancel Dialogue
+		if (Input.GetKeyDown("/")){
+			stopTheSubtitles ();
+			//			print("Stopped " + Time.time);
+		}
+
+		//hide/show subtitles
+		if (Input.GetKeyDown("h")){
+
+			Debug.Log ("Subtitles off:" + toggleSubtitles);
+
+			toggleSubtitles = !toggleSubtitles;
+
+			toggleSubtitlesControl ();
+		}
+
+	}
+
+
+}
+
