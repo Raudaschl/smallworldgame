7329b1df990ffb07802746d5e9a418c223e01ca6
Adrian Raudaschl <raudasch@gmail.com>
2017-04-24 15:59:24 +0100

    fixed lighting issues

--- Assets/scripts/spinTest.cs
+++ Assets/scripts/spinTest.cs
@@ -5,14 +5,8 @@ public class spinTest : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
-
 
 		iTween.ScaleTo(gameObject, iTween.Hash("scale", new Vector3(0.5f,0.5f,0.5f), "easeType", "easeInOutExpo", "loopType", "pingPong", "delay", .1));
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
 }
