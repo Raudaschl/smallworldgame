bc4050ba8039c40a4d3f7b2a580e3efc18a7284a
Adrian Raudaschl <raudasch@gmail.com>
2017-04-24 10:33:10 +0100

    room1 instructions

---
+++ Assets/hoverMove.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hoverMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		iTween.MoveBy (gameObject, iTween.Hash ("y", 0.2f, "easeType", "linear", "loopType", "pingPong"));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
